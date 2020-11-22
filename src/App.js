63087b0ff3c7f51bceeca731e3ae791b164a7e6a
hasan <hasan.maximmo@gmail.com>
2020-11-22 15:15:57 +0100

    state commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Img from "./image.jpg";
+import { Card } from "react-bootstrap";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: "HASAN BEN HAMOUDA",
+        bio: "Full Stack Web Developer",
+        imgSrc: { Img },
+        profession: "BI developer",
+        show: false,
+      },
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ count: this.state.count + 1 });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  handleClick = () => {
+    this.setState({
+      count: 0,
+      person: { ...this.state.person, show: !this.state.person.show },
+    });
+  };
+
+  toggledButon = () => {
+    const styleObject = {
+      color: "red",
+      textAlign: "center",
+      marginTop: "20px",
+    };
+    const styleObject2 = {
+      width: "18rem",
+      margin: "auto",
+      color: "grey",
+      marginTop: "50px",
+    };
+
+    return (
+      <div className="btnToggled" style={styleObject}>
+        {this.state.person.show ? (
+          <div>
+            <p>{this.state.count} seconds since componentDidMount</p>
+            <Card style={styleObject2}>
+              <Card.Img
+                variant="top"
+                src={Img}
+                style={{ width: "45%", margin: "30px auto 0px" }}
+              />
+              <Card.Body>
+                <Card.Title>{this.state.person.fullName}</Card.Title>
+                <Card.Text>{this.state.person.profession}</Card.Text>
+                <Card.Title>{this.state.person.bio}</Card.Title>
+              </Card.Body>
+            </Card>
+          </div>
+        ) : (
+          <p></p>
+        )}
+      </div>
+    );
+  };
+  render() {
+    return (
+      <div className="App">
+        <button
+          style={{ margin: "50px" ,color: "black"}}
+          onClick={() => {
+            this.handleClick();
+          }}
+          type="button"
+          className="btn btn-primary"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Afficher Profil{" "}
+        </button>
+        {this.toggledButon()}
+      </div>
+    );
+  }
 }
+
+App.propTypes = {};
 
 export default App;
